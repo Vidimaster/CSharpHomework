6724c2924756daa5cbe44c68f069fbfffeed0a8e
Vidimaster <vidimaster@yandex.com>
2023-04-23 16:30:16 +0700

    changes to folders

---
+++ Seminar 1/Task8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter number");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N > 1)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        if (i % 2 == 0)
+        {
+        System.Console.WriteLine($"{i}");
+    
+        }
+
+    }
+}
+        else
+        {
+        Console.WriteLine("Number has to be bigger than 1"); 
+        }
