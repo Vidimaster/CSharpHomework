355a61bdf113d5170ff87874c2bd11526ca14d7e
Vidimaster <vidimaster@yandex.com>
2023-04-29 17:59:50 +0700

    Task19

---
+++ Seminar 3/Task19/Program.cs
@@ -0,0 +1,27 @@
+﻿void ReadInt(string? str)
+{
+    bool a = false;
+    for (int i = 0; i < str.Length / 2; i++)
+    {
+        if (str![i] == str![str.Length - i - 1])
+        {
+            a = true;
+        }
+        else
+        {
+            a = false;
+            break;
+        }
+    }
+    if (a == true)
+    {
+        System.Console.WriteLine($"{str} -> да");
+    }
+    else
+    {
+        System.Console.WriteLine($"{str} -> нет");
+    }
+}
+
+System.Console.WriteLine("Введите пятизначное число");
+ReadInt(Console.ReadLine());
