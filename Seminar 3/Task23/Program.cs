e842c3804b05f90789a1754cca5bbe077da61479
Vidimaster <vidimaster@yandex.com>
2023-04-29 18:44:18 +0700

    task 23

---
+++ Seminar 3/Task23/Program.cs
@@ -0,0 +1,27 @@
+﻿void Cubed(string text)
+{
+    int N = Convert.ToInt32(text);
+    int[] result = new int[N + 1];
+    for (int i = 1; i <= N; i++)
+    {
+        result[i] = Convert.ToInt32(Math.Pow(i, 3));
+    }
+
+    System.Console.Write($"{N} -> ");
+    for (int i = 1; i <= N; i++)
+    {
+        System.Console.Write($"{result[i]}");
+        if (i == N)
+        {
+            System.Console.WriteLine();
+            break;
+        }
+        else
+        {
+            System.Console.Write($", ");
+        }
+    }
+}
+System.Console.WriteLine($"Введите число");
+Cubed(Console.ReadLine());
+
