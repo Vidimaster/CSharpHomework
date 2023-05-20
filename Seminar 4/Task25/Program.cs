107fe12347dd09e1b7bf745e3012f8d5cb5d4b86
Vidimaster <vidimaster@yandex.com>
2023-05-20 20:44:42 +0700

    Task25 done

---
+++ Seminar 4/Task25/Program.cs
@@ -0,0 +1,32 @@
+﻿bool isNatural(double n)
+{
+    if (n > 0)
+    {
+        return true;
+    }
+    else
+    {
+        System.Console.WriteLine("Число В должно быть натуральным!");
+        return false;
+    }
+}
+
+double Result(int[] ApowB)
+{
+    double result = Math.Pow(ApowB[0], ApowB[1]);
+    if (isNatural(ApowB[1]))
+    {
+        return result;
+    }
+    else
+    {
+        return 0;
+    }
+
+}
+
+int[] input = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+System.Console.WriteLine("Введите число А и В");
+System.Console.WriteLine(string.Join(", ", input) + " -> " + Result(input));
+
