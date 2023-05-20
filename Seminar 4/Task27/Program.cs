34853c62a40bd8bd528b6e477697c236fb837896
Vidimaster <vidimaster@yandex.com>
2023-05-20 22:57:01 +0700

    Task27 done

---
+++ Seminar 4/Task27/Program.cs
@@ -0,0 +1,14 @@
+﻿int Result(int n)
+{
+    int result = 0;
+    while (n > 0)
+    {
+        result = result + n % 10;
+        n = n / 10;
+    }
+   return result; 
+}
+
+System.Console.WriteLine("Введите число");
+int input = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"{input} -> {Result(input)}");
