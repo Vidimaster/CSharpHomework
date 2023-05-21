f4c4b3516358f948b8ab6d2ab2330557c452ff5c
Vidimaster <vidimaster@yandex.com>
2023-05-21 18:18:04 +0700

    task 34 and 36 done

---
+++ Seminar 5/Task34/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] FillArray(int size, int min, int max)
+{
+    Random rand = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] input)
+{
+    System.Console.Write("[" + string.Join(", ", input) + "]" + " -> ");
+}
+
+void PrintResult(int[] input)
+{
+    int count = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    System.Console.WriteLine($"{count}");
+}
+
+int[] array = FillArray(20, 100, 999);
+PrintArray(array);
+PrintResult(array);
---
+++ Seminar 5/Task36/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] FillArray(int size, int min, int max)
+{
+    Random rand = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] input)
+{
+    System.Console.Write("[" + string.Join(", ", input) + "]" + " -> ");
+}
+
+void PrintResult(int[] input)
+{
+    int count = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            count = count + input[i];
+        }
+    }
+    System.Console.WriteLine($"{count}");
+}
+
+int[] array = FillArray(6, 0, 10);
+PrintArray(array);
+PrintResult(array);
