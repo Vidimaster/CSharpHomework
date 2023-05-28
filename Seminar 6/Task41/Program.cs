9d4ce206ba140ba5870218d6ec44b13e669893c3
Vidimaster <vidimaster@yandex.com>
2023-05-29 01:36:03 +0700

    Task41 done

---
+++ Seminar 6/Task41/Program.cs
@@ -0,0 +1,16 @@
+﻿int CountNums (int[] input)
+{
+int length = input.Length;
+int count = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i] > 0) {count++;}
+}
+return count;
+}
+
+System.Console.WriteLine("Введите числа");
+int[] input = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+int countresult = CountNums(input);
+System.Console.WriteLine(string.Join(", ", input) + " -> " + countresult);
+System.Console.WriteLine($"В массиве {countresult} чисел больше нуля");
