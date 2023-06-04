dfeb3d823312890cff644a99fc02710729aad45c
Vidimaster <vidimaster@yandex.com>
2023-06-04 13:33:38 +0700

    Tasks54-60 done

---
+++ Seminar 8/Task54/Program.cs
@@ -0,0 +1,53 @@
+﻿int[,] FillMatrix(int row, int col, int leftRange, int rightRange)
+{
+    int[,] matrix = new int[row, col];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+
+    }
+}
+
+void SortRow(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - j - 1; k++)
+                if (matrix[i,k] < matrix[i,k + 1])
+                {
+                    int temp = matrix[i,k];
+                    matrix[i,k] = matrix[i,k + 1];
+                    matrix[i,k + 1] = temp;
+                }
+        }
+    }
+
+}
+
+int[,] matrix = FillMatrix(6, 6, 0, 20);
+System.Console.WriteLine();
+PrintMatrix(matrix);
+System.Console.WriteLine();
+SortRow(matrix);
+PrintMatrix(matrix);
---
+++ Seminar 8/Task60/Program.cs
@@ -0,0 +1,68 @@
+﻿int[] CreateArrayWithUniques(int size, int leftRange, int rightRange)
+{
+    int[] uniqueArr = new int[size];
+    int count = 0;
+    Random rand = new Random();
+    while (count < uniqueArr.Length)
+    {
+        bool elementrepeat = false;
+        int a = rand.Next(leftRange, rightRange + 1);
+        for (int i = 0; i < uniqueArr.Length; i++)
+        {
+            if (uniqueArr[i] == a)
+            {
+                elementrepeat = true;
+                break;
+            }
+        }
+        if (elementrepeat != true)
+        {
+            uniqueArr[count] = a;
+            count++;
+        }
+    }
+    count = 0;
+    return uniqueArr;
+}
+
+int[,,] FillMatrixWithUniques(int row, int col, int vol, int leftRange, int rightRange)
+{
+    int[,,] matrix = new int[row, col, vol];
+    int count = 0;
+    int[] uniqueArr = CreateArrayWithUniques(row * col * vol, leftRange, rightRange);
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = uniqueArr[count];
+                count++;
+            }
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                System.Console.Write($"{matrix[i, j, k]}");
+                System.Console.Write($"{(i, j, k)}");
+                System.Console.Write("\t");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
+int[,,] matrix = FillMatrixWithUniques(2, 2, 2, 10, 99);
+System.Console.WriteLine();
+PrintMatrix(matrix);
+System.Console.WriteLine();
+
