8adf3fc45b08dcbb32d6ceb0c5d7e0b4c7620c3c
Vidimaster <vidimaster@yandex.com>
2023-06-05 02:08:55 +0700

    Task64 done

---
+++ Seminar 9/Task64/Program.cs
@@ -0,0 +1,16 @@
+﻿int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void RecN(int n)
+{
+    if (n < 1) return;
+    System.Console.Write(n + " ");
+    RecN(n - 1);
+    
+}
+
+RecN(ReadInt("Введите число N"));
+
