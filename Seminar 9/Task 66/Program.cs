a27b5850bb508db155288f0117630be80de956e7
Vidimaster <vidimaster@yandex.com>
2023-06-05 02:09:18 +0700

    task 66 done

---
+++ Seminar 9/Task 66/Program.cs
@@ -0,0 +1,20 @@
+﻿int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int RecN(int M, int N)
+{
+    if (M == N)
+    {
+        return M;
+    }
+    else
+    {
+        return M + RecN(M + 1, N);
+    }
+}
+int sum = RecN(ReadInt("Введите число M"), ReadInt("Введите число N"));
+
+System.Console.WriteLine(sum);
