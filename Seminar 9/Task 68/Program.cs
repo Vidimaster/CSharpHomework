7469a7a4b659abf581fb4a60318740ae7c86371d
Vidimaster <vidimaster@yandex.com>
2023-06-05 02:10:21 +0700

    task 68 done

---
+++ Seminar 9/Task 68/Program.cs
@@ -0,0 +1,24 @@
+﻿int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int RecAck(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return RecAck(m - 1, 1);
+    }
+    else
+    {
+        return RecAck(m - 1, RecAck(m, n - 1));
+    }
+}
+int sum = RecAck(ReadInt("Введите число M"), ReadInt("Введите число N"));
+
+System.Console.WriteLine(sum);
